6b1d34b314cda3078d9ce0d1984546ca8de9d702
nabeatsu <tanabe.1478@gmail.com>
2020-05-18 22:18:13 +0900

    Replace BLoC based email sign in logic with ChangeNotifier

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timetrackerfirebase/Services/auth.dart';
-import 'package:timetrackerfirebase/app/sign_in/email_sign_in_form_bloc_based.dart';
+
+import 'email_sign_in_form_change_notifier.dart';
 
 class EmailSignInPage extends StatelessWidget {
   @override
@@ -16,7 +17,7 @@ class EmailSignInPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Card(
-            child: EmailSignInFormBlocBased.create(context),
+            child: EmailSignInFormChangeNotifier.create(context),
           ),
         ),
       ),
--- lib/app/sign_in/validators.dart
+++ lib/app/sign_in/validators.dart
@@ -13,5 +13,5 @@ class EmailAndPasswordValidators {
   final StringValidator emailValidator = NonEmptyStringValidator();
   final StringValidator passwordValidator = NonEmptyStringValidator();
   final String invalidEmailErrorText = 'Email can\'t be empty';
-  final String invalidPasswordErrorText = 'Passwrd can\'t be empty';
+  final String invalidPasswordErrorText = 'Password can\'t be empty';
 }
